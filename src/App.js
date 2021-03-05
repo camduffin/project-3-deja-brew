358b4198995f1fc85a98d94f3b5b104b99fdd418
cameronduffin <camduffin@gmail.com>
2021-03-05 17:56:47 -0500

    added brewery component and accessed necessary api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import { FaBeer } from 'react-icons/fa';
+import axios from 'axios';
+import Breweries from './Breweries.js';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  // const [brewery, setBrewery] = useState([]);
+
+  function getBreweries() {
+    axios({
+      method: 'GET',
+      url: 'https://api.openbrewerydb.org/breweries',
+      dataResponse: 'JSON',
+      params: {
+        by_city: 'New York',
+        format: 'json',
+      }
+    }).then(response => {
+      const breweryData = response.data;
+      console.log(breweryData);
+
+      // infinite loop on page load
+      // setBrewery(breweryData);
+    })
+  }
+
+  getBreweries();
+
   return (
     <div className="App">
       <header>
@@ -12,9 +38,18 @@ function App() {
           <h1>Déjà Brew</h1>
         </div>
         <nav>
-          <p>Created at<a href="https://junocollege.com/" target="_blank" rel="noopener">Juno College</a><span>by Cam Duffin</span></p>
+          <p>Created at<a href="https://junocollege.com/" target="_blank" rel="noreferrer noopener">Juno College</a><span>by Cam Duffin</span></p>
         </nav>
       </header>
+
+      <form action="">
+        <input type="text" id="" placeholder="Search for breweries by city..."/>
+
+        <label htmlFor=""></label>
+        <button>Search</button>
+      </form> 
+      <Breweries />
+
     </div>
   );
 }
---
+++ src/Breweries.js
@@ -0,0 +1,15 @@
+import './breweries.css';
+
+function Breweries() {
+    return(
+            <ul>
+                <li>
+                    <h2>Brewery</h2>
+                    <p>123 Brewery Street</p>
+                    <p>www.brewery.com</p>
+                </li>
+            </ul>
+    )
+}
+
+export default Breweries;
