dedf5403c34c76492e749f03d27e1a0f666a719b
cameronduffin <camduffin@gmail.com>
2021-03-05 15:33:49 -0500

    adding basic styles to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { FaBeer } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Déjà Brew</h1>
+      <header>
+        <div className="heading-container">
+          <div className="beer-logo">
+            <FaBeer />
+          </div>
+          <h1>Déjà Brew</h1>
+        </div>
+        <nav>
+          <p>Created at<a href="https://junocollege.com/" target="_blank" rel="noopener">Juno College</a><span>by Cam Duffin</span></p>
+        </nav>
+      </header>
     </div>
   );
 }
